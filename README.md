6a3d64cc3088734b1c83fe188b9402333d1a1eb7
KacperKornas <116450906+KacperKornas@users.noreply.github.com>
2025-05-29 09:44:29 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# campaign-manager-api
+Spring Boot REST API for managing marketing campaigns with Spring Data JPA and in-memory H2 database.
---
+++ src/test/java/com/kacper/campaign_manager/CampaignManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kacper.campaign_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampaignManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
