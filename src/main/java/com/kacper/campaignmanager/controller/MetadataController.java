0a415e7d1e10e0c8a04ed5ba693c5926222036a9
KacperKornas <kornaskacper1@gmail.com>
2025-05-30 21:44:51 +0200

    Create MetadataController

---
+++ src/main/java/com/kacper/campaignmanager/controller/MetadataController.java
@@ -0,0 +1,23 @@
+package com.kacper.campaignmanager.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class MetadataController {
+
+    @GetMapping("/keywords")
+    public List<String> GetKeywords() {
+        return List.of("marketing", "advertising", "digital", "social media",
+                "SEO", "PPC", "content marketing", "email marketing");
+    }
+
+    @GetMapping("/towns")
+    public List<String> GetTowns() {
+        return List.of("WARSAW","KRAKOW","GDANSK","WROCLAW","POZNAN");
+    }
+}
